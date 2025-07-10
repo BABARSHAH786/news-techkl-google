e1e24eb1267cabae0333f53ef98349d56526528f
BABARSHAH786 <syedbabarali640@gmail.com>
2025-07-10 17:04:40 +0500

    comment export

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // output: 'export',
     // output: "standalone", // ✅ required for SSR deployment menay rendder k liye use kiya
 
   eslint: {
